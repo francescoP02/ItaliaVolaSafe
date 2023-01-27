dfc29386c5bc179e4e71f3ed113daa0d2cc3bffd
francescoP02 <file.partipilo@gmail.com>
2023-01-27 17:59:08 +0100

    Modify navbar link and align navbar link on right

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,16 +23,16 @@
 
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
           <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-              <img src="" alt="Bootstrap" width="30" height="24">
+            <a class="navbar-brand" href="{{ url('/') }}">
+              <img src="" alt="{{ config('app.name') }}" width="30" height="24">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
-              <ul class="navbar-nav">
+              <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                  <a class="nav-link" aria-current="page" href="#">Home</a>
+                  <a class="nav-link" aria-current="page" href="{{ url('/') }}">Home</a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link" href="#">Richiedi il tuo rimborso</a>
